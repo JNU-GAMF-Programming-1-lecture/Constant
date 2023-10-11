30e4b9c02394fe5a727d95a9917a94db9ce560a9
jzscsaba <johanyak.csaba@nje.hu>
2023-10-11 19:22:59 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+	char c = 'A';
+	cout << bitset<8>(c) << endl << endl;
+
+
+	const int s = 1;
+	int x = 3;
+
+	cout << 3 * 7 % 2 + 5 / 3;
+
+	return 0;
+}
