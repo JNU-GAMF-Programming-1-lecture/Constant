d56a5f8fc8a1ce5eae29391876f77aab0ef956e3
Z. Csaba Johanyák <39920181+jzscsaba@users.noreply.github.com>
2024-09-20 12:06:21 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,14 @@
-#include <iostream>
-#include <bitset>
-
+# include <iostream>
 using namespace std;
-
+const double PI = 3.1415;     // Capital letters are not compulsory
 int main()
 {
-	char c = 'A';
-	cout << bitset<8>(c) << endl << endl;
-
-
-	const int s = 1;
-	int x = 3;
-
-	cout << 3 * 7 % 2 + 5 / 3;
-
+	int r;
+	cout << "Radius" << endl;
+	cin >> r;
+	cout << "Perimeter of the circle: " <<2 * r * PI << endl;
+	cout << "Area of the circle: " <<r * r * PI << endl;
+	cout << "Surface of the sphere: " <<4*r * r * PI << endl;
+	cout << "Volume of the sphere: "<<4.0/3.0*r*r* r*PI<< endl;
 	return 0;
 }
